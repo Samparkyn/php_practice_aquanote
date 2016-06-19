b0842ceb3a01d2f26661bbaad160e5c28b7464cf
Samparkyn <samparkyn101@gmail.com>
2016-06-19 10:47:26 +0100

    refactored to use render function

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -14,13 +14,9 @@ class GenusController extends Controller {
   
   public function showAction($genusName){
     
-     $templating = $this->container->get('templating');
-     
-     $html = $templating->render('genus/show.html.twig', array(
-            'name' => $genusName
-        ));
-    
-    return new Response($html);
+    return $this->render('genus/show.html.twig', array(
+           'name' => $genusName
+       ));
     
   }
 }
