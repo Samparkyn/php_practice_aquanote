31dbb9bc3d1874db3158f6f5ef3b6a570a390423
Samparkyn <samparkyn101@gmail.com>
2016-06-19 10:29:54 +0100

    added Framework bundle to extend controller and added code to be able to render a template

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -4,8 +4,9 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class GenusController {
+class GenusController extends Controller {
   
   /**
   * @Route("/genus/{genusName}")
@@ -13,7 +14,13 @@ class GenusController {
   
   public function showAction($genusName){
     
-    return new Response('The Genus: ' .$genusName);
+     $templating = $this->container->get('templating');
+     
+     $html = $templating->render('genus/show.html.twig', array(
+            'name' => $genusName
+        ));
+    
+    return new Response($html);
     
   }
 }
